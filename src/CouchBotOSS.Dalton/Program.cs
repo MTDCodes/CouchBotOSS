d3f7dc93ce7402fa785149e46bb64b9e9d96bbd8
MattTheDev <me@mattthedev.codes>
2023-04-22 14:37:04 -0500

    Added EF Core Context, DiscordUserAccessor, and AutoMapper/DTO

--- src/CouchBotOSS.Dalton/Program.cs
+++ src/CouchBotOSS.Dalton/Program.cs
@@ -1,13 +1,18 @@
 using CouchBotOSS.Dalton.Models;
 using CouchBotOSS.Dalton.Services.Hosted;
+using CouchBotOSS.Shared.Mapping;
 using CouchBotOSS.Shared.Models.Bot;
+using MTD.CouchBot.Accessors.Contracts;
+using MTD.CouchBot.Accessors.Implementations;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
+        services.AddAutoMapper(typeof(MappingProfile));
         services.Configure<PatreonConfiguration>(context.Configuration.GetSection(nameof(PatreonConfiguration)));
         services.Configure<TimerConfiguration>(context.Configuration.GetSection(nameof(TimerConfiguration)));
         services.AddMemoryCache();
+        services.AddSingleton<IPatreonAccessor, PatreonAccessor>();
         services.AddHostedService<PatreonWorker>();
     })
     .Build();
---
+++ src/CouchBotOSS.Shared/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CouchBotOSS.Data.Models;
+using CouchBotOSS.Shared.Dtos;
+
+namespace CouchBotOSS.Shared.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<DiscordUser, DiscordUserDto>().ReverseMap();
+        }
+    }
+}
