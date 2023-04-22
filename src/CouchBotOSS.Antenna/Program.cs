06ed5835599377b87a367c1928d7dfe749e19af9
MattTheDev <me@mattthedev.codes>
2023-04-21 22:24:55 -0500

    Add Dalton, Antenna, and Accessor projects

---
+++ src/CouchBotOSS.Antenna/Program.cs
@@ -0,0 +1,10 @@
+using CouchBotOSS.Antenna;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
---
+++ src/CouchBotOSS.Dalton/Program.cs
@@ -0,0 +1,10 @@
+using CouchBotOSS.Dalton;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
